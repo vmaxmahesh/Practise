66df19c082a2c8ff024d2cc7df57f1348d6b68cd
vmaxmahesh <vmax.mahesh@gmail.com>
2023-02-20 20:13:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practise';
+
+  data={
+    title :'Mahesh'
+  };
+
+
+  changeInput=(newTitle:string)=>{
+      this.data.title=newTitle;
+  }
 }
